1d1d658c05c7482216c96893362cdba0c0a7cdff
Husnuye <husnuye_@hotmail.com>
2025-07-03 21:11:54 +0300

    Update ProductPage.cs after resolving conflicts

--- WebTests/Pages/ProductPage.cs
+++ WebTests/Pages/ProductPage.cs
@@ -133,11 +133,8 @@ namespace WebTests.Pages
         /// Adds a product to the cart by clicking 'Add' button, selecting first available size,
         /// and clicking 'Complete Order' button if present.
         /// </summary>
-<<<<<<< HEAD
 
 
-=======
->>>>>>> main
         public void AddProductToCartWithSize()
         {
             TestContext.WriteLine("[INFO] Starting process to add product to cart.");
@@ -184,7 +181,6 @@ namespace WebTests.Pages
                             return;
                         }
                     }
-<<<<<<< HEAD
 
                     // 🔽 Ek: Popup'ı kapat
                     try
@@ -201,8 +197,6 @@ namespace WebTests.Pages
                         TestContext.WriteLine($"[WARN] Popup removal failed: {ex.Message}");
                     }
                     // 🔼 Ek Bitiş
-=======
->>>>>>> main
                 }
                 else
                 {
@@ -230,17 +224,8 @@ namespace WebTests.Pages
                 return;
             }
         }
-<<<<<<< HEAD
 
 
-
-
-
-
-
-
-=======
->>>>>>> main
         /// <summary>
         /// Helper method that waits for an element to be visible within a timeout period.
         /// </summary>
